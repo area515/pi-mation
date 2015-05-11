295e201114df92f0f8ce58292da52ef9dbe9442f
ergobot <stobryan@gmail.com>
2015-05-11 06:27:18 +0000

    show video_processing_screen fix

--- pi-mation/pi-mation.py
+++ pi-mation/pi-mation.py
@@ -108,10 +108,12 @@ def make_movie():
     camera.stop_preview()
     # display the video processing screen
     screen.blit(video_pic_fix, (0, 0))
-    pygame.quit()
+    pygame.display.update()
+    #pygame.quit()
     print "\nQuitting Pi-Mation to transcode your video.\nWarning: this will take a long time!"
     print "\nOnce complete, write 'omxplayer video.mp4' in the terminal to play your video.\n"
     os.system("avconv -r " + str(fps) + " -i " + str((os.path.join('pics', 'image_%d.jpg'))) + " -vcodec libx264 video.mp4")
+    pygame.quit()
     print "\nMade movie -- debug.\n"
     sys.exit(410)
     
